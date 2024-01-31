261d2def9bd4d04aa6ac22f81f32c0dec1f35848
Luxlator <51272308+Luxlator@users.noreply.github.com>
2024-01-31 16:22:27 -0500

    Update send.lua

--- send.lua
+++ send.lua
@@ -9,7 +9,12 @@ for i = 1, #periList do
 end
 
 --write the message
+write("To cancel press and hold 'CTRL - T'")
+write()
 write("what would you like to say?  ")
+if(message == stop) then
+
+
 local message = read()
 write("who are you sending it to?  ")
 local target = read() --ask for target
