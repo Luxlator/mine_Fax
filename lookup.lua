902c2eba02a5fcb0f2fe68d8967d6dd26081bf65
Luxlator <51272308+Luxlator@users.noreply.github.com>
2024-01-31 16:28:12 -0500

    Add files via upload

--- lookup.lua
+++ lookup.lua
@@ -9,11 +9,3 @@ for i = 1, #periList do
 	print("I have a "..peripheral.getType(periList[i]).." attached as \""..periList[i].."\".")
 	i= i+1
 end
-
-print("To start recieving messages enter 'bg recieve-all' to run in the background.")
-print ()
-print("To start sending messages enter 'fg send' to open a tab.")
-print()
-print("To play snake enter 'fg worm'! The goal is to not eat yourself!")
-print()
-print("To clear the screen enter 'clear'")
